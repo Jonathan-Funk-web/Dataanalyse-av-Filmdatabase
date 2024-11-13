3bfe9d735fcfae1286316915dd64c5947f1de693
Jonathan-Funk-web <Jonathan.torp@funkweb.org>
2024-11-13 10:07:35 +0000

    Added a error handler if you get an API error

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-import os,requests,pydoc,json
+import os,requests,pydoc,json,sys
 from dotenv import load_dotenv, dotenv_values 
 
+Get_new_data = True #If true, does a API request, if false uses data in data.json
+media_list = [] 
+
+if Get_new_data:
+    load_dotenv() 
+    Auth_key = os.getenv("MOVIEDB_APP_AUTH_DOMAIN")
+
+    #API request 
+    #url = "https://api.themoviedb.org/3/authentication"
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+    #TODO: make this only get data if data.json is empty (as to reduce the amount of searches)
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer %s" % Auth_key
+    }
+
+    response = requests.get(url, headers=headers)
+    response_text = json.loads(response.text)
+    response_text_formatted = json.dumps(response_text, indent=4)
+    if not response_text.get("success", True):
+        sys.exit("API conection failed, error: %s\n%s" % (response_text["status_code"],response_text["status_message"]))
+        
 
 
-load_dotenv() 
-Auth_key = os.getenv("MOVIEDB_APP_AUTH_DOMAIN")
+    with open("data.json", "w") as f:
+        f.write(response_text_formatted)
+else:
+    print("Did not retreve new data, used old data. \n")
+    with open("data.json") as f:
+        response_text = json.load(f)
 
 
-#API request 
-#url = "https://api.themoviedb.org/3/authentication"
-url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+data = response_text["results"]
 
-#TODO: make this only get data if data.json is empty (as to reduce the amount of searches)
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer %s" % Auth_key
-}
+for i in range(0,len(data)):
+    if data[i]["adult"]:
+        print("is adult")
+        media_list.append(data[i]["title"])
+    #print(data[i]["title"])
 
-response = requests.get(url, headers=headers)
-response_text = json.loads(response.text)
-response_text_formatted = json.dumps(response_text, indent=4)
-#if response_text["success"]:
-#    print("API conection succsesfull")
-#else:
-#    print("API conection failed, error: %s\n%s" % (response_text["status_code"],response_text["status_message"]))
-
-
-
-with open("data.json", "w") as f:
-    f.write(response_text_formatted)
-
-print(response_text["page"])
+print(media_list)
