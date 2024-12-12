4c46ca2eed82f4077537b0a755ed54dcd06a2a8b
Jonathan-Funk-web <Jonathan.torp@funkweb.org>
2024-12-12 10:11:34 +0000

    made get_production

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def filter_non_basic_data():
     pass
     #This function will use the functions below to filter the data one final time, and get what i will use for the graphs and such. Soon™
 
-#TODO make it so that these functions does not have to open then close the file every time.
+#TODO make it so that these functions does not have to open then close the file every time. Also make the media_id be able to be a int not just str.
 def get_votes(import_location:str, media_id:str, weighted:bool = False) -> float:
     """
     Gets the average votes for the media.
@@ -270,6 +270,93 @@ def get_images(import_location:str, media_id:str, image_type:str="poster", image
     else:
         return "https://image.tmdb.org/t/p/" + image_size + data[media_id]["Details"][image_type]
 
+def get_countries(import_location:str, media_id:str) -> str:
+    """
+    Gets the country the media originates from.
+    
+    Args:
+        import_location (str): Location for the .json file with the movie data.
+        media_id (str): The media you want the income for.
+    Returns:
+        str: The country the media originates from.
+    """
+    with open(import_location, "r") as file:
+        data = json.load(file)
+
+    return data[media_id]["Details"]["origin_country"][0]
+
+def get_languages(import_location:str, media_id:str) -> str:
+    """
+    Gets the languages used in the media.
+    
+    Args:
+        import_location (str): Location for the .json file with the movie data.
+        media_id (str): The media you want the income for.
+    
+    Returns:
+        str: language
+    """
+    with open(import_location, "r") as file:
+        data = json.load(file)
+
+    return data[media_id]["Details"]["original_language"]
+
+def get_popularity(import_location:str, media_id:str) -> float:
+    """
+    Gets the popularity rating for the media.
+    
+    Args:
+        import_location (str): Location for the .json file with the movie data.
+        media_id (str): The media you want the income for.
+    
+    Returns:
+        float: Popularity rating
+    """
+    with open(import_location, "r") as file:
+        data = json.load(file)
+
+    return data[media_id]["Details"]["popularity"]
+
+def get_production(import_location:str, media_id:str, info_wanted:str="name", company_nr:int|None=None) -> str|list[str]|int:
+    """
+    Gets information about the production companies that produced the media.
+    
+    Args:
+        import_location (str): Location for the .json file with the movie data.
+        media_id (str): The media you want the income for.
+        info_wanted (str): The information wanted. (Supported parameters: \n `logo` gives the url for the logo.\n `name` gives the companies name.\n `country` gives the origin country for the company.\n `amount` gives the amount of production companies involved with the media. \n `name_list` gives the list of names for all the companies involved with the media. \n `country_list` gives the list of all the different origin countries for the companies.)
+        company_nr (int): Gives the information for company number `company_nr`. Is not needed for `amount`, `name_list`, `all_countries`.
+    Returns:
+        str: Information wanted (if used `info_wanted=logo` or `info_wanted=name` )
+        list[str]: Information wanted (if used `info_wanted=name_list` or `info_wanted=country_list`)
+        int: Amount of companies (if used `info_wanted=amount`)
+    """
+    with open(import_location, "r") as file:
+        data = json.load(file)
+
+
+    if info_wanted == "logo":
+        if data[media_id]["Details"]["production_companies"][company_nr]["logo_path"] == None:
+            print("No url for logo found for company " + data[media_id]["Details"]["production_companies"][company_nr]["name"])
+            return
+        return "https://image.tmdb.org/t/p/original" + data[media_id]["Details"]["production_companies"][company_nr]["logo_path"]
+    if info_wanted == "name":
+        return data[media_id]["Details"]["production_companies"][company_nr]["name"]
+    if info_wanted == "country":
+        return data[media_id]["Details"]["production_companies"][company_nr]["origin_country"]
+    if info_wanted == "amount":
+        return len(data[media_id]["Details"]["production_companies"])
+    if info_wanted == "name_list":
+        name_list = []
+        for i in range(len(data[media_id]["Details"]["production_companies"])):
+            name_list.append(data[media_id]["Details"]["production_companies"][i]["name"])
+        return name_list
+    if info_wanted == "country_list":
+        country_list = []
+        for i in range(len(data[media_id]["Details"]["production_companies"])):
+            if data[media_id]["Details"]["production_companies"][i]["origin_country"] not in country_list:
+                country_list.append(data[media_id]["Details"]["production_companies"][i]["origin_country"])
+        return country_list
 
 def get_PLACEHOLDER(import_location:str, media_id:str) -> None:
     """
@@ -289,4 +376,4 @@ def get_PLACEHOLDER(import_location:str, media_id:str) -> None:
 
     #This is not meatn to be a function, just a template to quickly make new "get_..." functions
 
-print(get_images("Data/extra_media_details.json","1035048","poster","original",True))
+print(get_production("Data/extra_media_details.json","1241982","all_countries"))
